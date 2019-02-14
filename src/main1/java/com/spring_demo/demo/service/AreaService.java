395daad32cf1c7ca9872818595c1e816d5b0aee9
flywithbug <369495368@qq.com>
2019-02-14 20:33:34 +0800

    no message

---
+++ src/main1/java/com/spring_demo/demo/service/AreaService.java
@@ -0,0 +1,46 @@
+package com.spring_demo.demo.service;
+
+import com.spring_demo.demo.entity.Area;
+
+import java.util.List;
+
+public interface AreaService {
+
+    /**
+     * 列出区域列表
+     *
+     * @return areaList
+     */
+    List<Area> queryArea();
+
+    /**
+     * 根据Id列出具体区域
+     *
+     * @return area
+     */
+    Area queryAreaById(int areaId);
+
+    /**
+     * 插入区域信息
+     *
+     * @param area
+     * @return
+     */
+    boolean insertArea(Area area);
+
+    /**
+     * 更新区域信息
+     *
+     * @param area
+     * @return
+     */
+    boolean updateArea(Area area);
+
+    /**
+     * 删除区域信息
+     *
+     * @param areaId
+     * @return
+     */
+    boolean deleteArea(int areaId);
+}
